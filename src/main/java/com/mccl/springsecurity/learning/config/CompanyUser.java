856b4a0e6218b54a6ba731a68dc6198f50abb48a
Miguel Ccantuta <mccantuta@yahoo.com>
2017-09-22 10:59:42 -0500

    Add custom User Detail

---
+++ src/main/java/com/mccl/springsecurity/learning/config/CompanyUser.java
@@ -0,0 +1,99 @@
+package com.mccl.springsecurity.learning.config;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class CompanyUser implements UserDetails {
+    private String userName;
+    private String password;
+    private boolean accountNonExpired;
+    private boolean accountNonLocked;
+    private boolean credentialsNonExpired;
+    private boolean enabled;
+    private List<GrantedAuthority> authorities = new ArrayList<>();
+
+    public CompanyUser() {
+        accountNonExpired = true;
+        accountNonLocked = true;
+        credentialsNonExpired = true;
+        enabled = true;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setAccountNonExpired(boolean accountNonExpired) {
+        this.accountNonExpired = accountNonExpired;
+    }
+
+    public void setAccountNonLocked(boolean accountNonLocked) {
+        this.accountNonLocked = accountNonLocked;
+    }
+
+    public void setCredentialsNonExpired(boolean credentialsNonExpired) {
+        this.credentialsNonExpired = credentialsNonExpired;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public void setAuthorities(List<GrantedAuthority> authorities) {
+        this.authorities = authorities;
+    }
+
+    public void addRole(String... roles) {
+        for (String role : roles) {
+            authorities.add(new SimpleGrantedAuthority(role));
+        }
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return credentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
--- src/main/java/com/mccl/springsecurity/learning/config/WebSecurityConfig.java
+++ src/main/java/com/mccl/springsecurity/learning/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.mccl.springsecurity.learning.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,15 +18,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/public/**").anonymous()
                 .antMatchers("/resource").hasRole("USER_RESOURCE")
                 .anyRequest().authenticated().and()
-                .formLogin().loginPage("/login.html").successForwardUrl("/resource").permitAll().and()
+                //.formLogin().loginPage("/login.html").successForwardUrl("/resource").permitAll().and()
+                .formLogin().permitAll().and()
                 .csrf().disable();
     }
 
     @Bean
     public UserDetailsService userDetailsService() {
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withUsername("user").password("password").roles("USER").build());
-        manager.createUser(User.withUsername("resource").password("password").roles("USER_RESOURCE").build());
+        /*manager.createUser(User.withUsername("user").password("password").roles("USER").build());
+        manager.createUser(User.withUsername("resource").password("password").roles("USER_RESOURCE").build());*/
+        CompanyUser companyUser = new CompanyUser();
+        companyUser.setUserName("user");
+        companyUser.setPassword("password");
+        companyUser.addRole("USER");
+        manager.createUser(companyUser);
+        CompanyUser resourceUser = new CompanyUser();
+        resourceUser.setUserName("resource");
+        resourceUser.setPassword("password");
+        resourceUser.addRole("ROLE_USER_RESOURCE");
+        manager.createUser(resourceUser);
+
         return manager;
     }
+
+
 }
